3e84365e130e5e34fc24f87e83fb1e01a158330a
Erika Heidi <erika.christina@gmail.com>
2013-10-10 14:29:46 +0200

    Added basic filter applying

--- src/Placephant/Controller/ImageController.php
+++ src/Placephant/Controller/ImageController.php
@@ -2,7 +2,10 @@
 
 namespace Placephant\Controller;
 
+use Imanee\Exception\FilterNotFoundException;
 use Imanee\Imanee;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @package Placephant
@@ -12,20 +15,22 @@ class ImageController extends \Flint\Controller\Controller
     /**
      * @param integer $width
      * @param integer $height
-     * @param boolean $bw
+     * @param Request $request
      * @return Response
      */
-    public function showAction($width, $height = 0, $bw = false)
+    public function showAction($width, $height = 0, Request $request)
     {
         $resource = $this->get('resources')->getRandom();
         $height = $height ?: $width;
 
-
         $imanee = (new Imanee($resource))
             ->thumbnail(abs($width), abs($height), true);
 
-        if ($bw) {
-           /* apply black and white filter */
+        if ($request->query->has('filter')) {
+
+            try {
+                $imanee->applyFilter('filter_' . $request->query->get('filter'));
+            } catch (FilterNotFoundException $e) {};
         }
 
         return $this->text($imanee->output(), 200, array(
